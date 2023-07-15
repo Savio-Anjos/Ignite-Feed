a47062837a4348abd643cc8540d8cbd0900c8bd7
savio <savioanjos2020@gmail.com>
2023-07-15 20:52:07 -0300

    :lipstick: feat: Configurando estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-function App() {
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
+
+export function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div>
+      <Header />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolores recusandae dignissimos consequatur ut debitis impedit delectus reprehenderit reiciendis, aspernatur autem enim nulla magnam neque tempore. Necessitatibus officiis eaque non."
+      />
+    </div>
   );
 }
-
-export default App;

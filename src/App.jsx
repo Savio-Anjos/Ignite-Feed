5b9d8134e47f3938b757cd2970e6b11b60bc3f46
savio <savioanjos2020@gmail.com>
2023-07-16 14:30:29 -0300

    :sparkles: feat: Criando componente SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolores recusandae dignissimos consequatur ut debitis impedit delectus reprehenderit reiciendis, aspernatur autem enim nulla magnam neque tempore. Necessitatibus officiis eaque non."
-      />
+      <div className={styles.whapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolores recusandae dignissimos consequatur ut debitis impedit delectus reprehenderit reiciendis, aspernatur autem enim nulla magnam neque tempore. Necessitatibus officiis eaque non."
+          />
+
+          <Post
+            author="Pedro Henrique"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolores recusandae dignissimos consequatur ut debitis impedit delectus reprehenderit reiciendis, aspernatur autem enim nulla magnam neque tempore. Necessitatibus officiis eaque non."
+          />
+        </main>
+      </div>
     </div>
   );
 }

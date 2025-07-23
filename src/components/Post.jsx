df8b9da676582563af752d202f258069cbf16c63
Savio Anjos <savioanjos2020@gmail.com>
2025-07-22 23:00:21 -0300

    feat: :sparkles: Understanding the key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,10 +53,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}> {line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -80,8 +80,8 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        {comments.map((comment, index) => {
-          return <Comment key={index} content={comment} />;
+        {comments.map((comment) => {
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
